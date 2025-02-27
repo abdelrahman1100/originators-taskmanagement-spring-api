9669bf2e98c95f153a879207d59b70aa0aa95841
Abdelrahman Tarek <Abdulrahmantarek0.1@gmail.com>
2025-02-27 17:16:02 +0200

    fixed auth error

---
+++ to-do-list/src/main/java/com/example/to_do_list/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.example.to_do_list.repository;
+
+
+import com.example.to_do_list.models.UserEntity;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends MongoRepository<UserEntity,String> {
+    Optional<UserEntity> findByUsername(String username);
+    Boolean existsByUsername(String username);
+}
