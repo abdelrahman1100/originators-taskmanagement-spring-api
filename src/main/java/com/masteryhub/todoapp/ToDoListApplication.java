0ced5eccb0486e9e384d3c04b67a1ddd209e0d7c
Abdelrahman Tarek <Abdulrahmantarek0.1@gmail.com>
2025-03-12 20:51:59 +0200

    fixed error

--- src/main/java/com/masteryhub/todoapp/ToDoListApplication.java
+++ src/main/java/com/masteryhub/todoapp/ToDoListApplication.java
@@ -7,6 +7,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ToDoListApplication {
   public static void main(String[] args) {
     SpringApplication.run(ToDoListApplication.class, args);
-    int a;
   }
 }
