f88ef05cd42108cb344ba6c72ad0990e1a066a1d
Abdelrahman Tarek <Abdulrahmantarek0.1@gmail.com>
2025-03-12 20:18:31 +0200

    try pre commit

--- src/main/java/com/masteryhub/todoapp/ToDoListApplication.java
+++ src/main/java/com/masteryhub/todoapp/ToDoListApplication.java
@@ -4,22 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ToDoListApplication { public static void main(String[] args){SpringApplication.run(
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-                ToDoListApplication.class,                    args);
+public class ToDoListApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(
+                ToDoListApplication.class, args);
     }
 }
