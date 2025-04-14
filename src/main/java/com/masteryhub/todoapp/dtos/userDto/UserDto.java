3c1f6d864c9f47c01ac8eca54c5a2bbfff7db079
Abdelrahman Tarek <Abdulrahmantarek0.1@gmail.com>
2025-04-14 14:49:16 +0200

    fixed response bug

--- src/main/java/com/masteryhub/todoapp/dtos/userDto/UserDto.java
+++ src/main/java/com/masteryhub/todoapp/dtos/userDto/UserDto.java
@@ -1,5 +1,10 @@
 package com.masteryhub.todoapp.dtos.userDto;
 
+import com.masteryhub.todoapp.models.settingsModel.Settings;
+import com.masteryhub.todoapp.models.userModel.ProfileImage;
+import com.masteryhub.todoapp.models.userModel.UserEntity;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,6 +13,42 @@ import lombok.Setter;
 @Getter
 @AllArgsConstructor
 public class UserDto {
+  private String id;
+
   private String username;
+
+  private String fullName;
+
   private String email;
+
+  private String phoneNumber;
+
+  private String address;
+
+  private String password;
+
+  private List<String> todos;
+
+  private List<String> friends;
+
+  private Integer tokenVersion;
+
+  private ProfileImage profileImage;
+
+  private Settings settings;
+
+  public UserDto(UserEntity user) {
+    this.id = user.getId();
+    this.username = user.getUsername();
+    this.fullName = user.getFullName();
+    this.email = user.getEmail();
+    this.phoneNumber = user.getPhoneNumber();
+    this.address = user.getAddress();
+    this.password = user.getPassword();
+    this.todos = new ArrayList<>();
+    this.friends = new ArrayList<>();
+    this.tokenVersion = user.getTokenVersion();
+    this.profileImage = user.getProfileImage();
+    this.settings = user.getSettings();
+  }
 }
--- src/main/java/com/masteryhub/todoapp/handlers/CustomOAuth2SuccessHandler.java
+++ src/main/java/com/masteryhub/todoapp/handlers/CustomOAuth2SuccessHandler.java
@@ -2,7 +2,6 @@ package com.masteryhub.todoapp.handlers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.masteryhub.todoapp.dtos.userDto.AuthenticationResponseDto;
-import com.masteryhub.todoapp.dtos.userDto.UserDto;
 import com.masteryhub.todoapp.models.userModel.UserEntity;
 import com.masteryhub.todoapp.repository.UserRepository;
 import com.masteryhub.todoapp.security.JwtGenerator;
@@ -61,7 +60,8 @@ public class CustomOAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHa
     String token = jwtGenerator.generateToken(userDetails);
 
     AuthenticationResponseDto authResponse =
-        new AuthenticationResponseDto(token, new UserDto(user.getUsername(), user.getEmail()));
+        new AuthenticationResponseDto(
+            token, userRepository.findByUsername(userDetails.getUsername()).get());
     response.setContentType("application/json");
     response.setCharacterEncoding("UTF-8");
     response.getWriter().write(objectMapper.writeValueAsString(authResponse));
